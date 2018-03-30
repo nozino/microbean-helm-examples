a0ea6e0d5def518f24a013e4dad5fc46771b903a
비디 노진호 <nojinho@bluedigm.com>
2018-03-30 11:52:56 +0900

    add helm delete example and change name of classes

--- src/main/java/org/nozino/microbean/helm/example/HelmDelete.java
+++ src/main/java/org/nozino/microbean/helm/example/HelmDelete.java
@@ -1,26 +1,15 @@
-/*
- * by nozino (nozino AT gmail.com)
- * chart name : stable/mariadb (by bitnami) 
- */
-
 package org.nozino.microbean.helm.example;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
-
-import org.microbean.helm.chart.URLChartLoader;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-import hapi.chart.ChartOuterClass.Chart;
 import hapi.release.ReleaseOuterClass.Release;
 
-import hapi.services.tiller.Tiller.InstallReleaseRequest;
-import hapi.services.tiller.Tiller.InstallReleaseResponse;
-import hapi.chart.ConfigOuterClass.Config.Builder;
+import hapi.services.tiller.Tiller.UninstallReleaseRequest;
+import hapi.services.tiller.Tiller.UninstallReleaseResponse;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
@@ -31,11 +20,11 @@ import org.microbean.helm.Tiller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ChartInstallation {
-	private static final Logger logger = LoggerFactory.getLogger(ChartInstallation.class);
-
+public class HelmDelete {
+	private static final Logger logger = LoggerFactory.getLogger(HelmDelete.class);
+	
 	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
-		if (args.length < 3) {
+		if (args.length < 2) {
 			System.out.println("Usage: cmd <kubernetes-config> <release-name> <pvc>");
 			return;
 		}
@@ -48,9 +37,9 @@ public class ChartInstallation {
 		}
 		
 		String releaseName = args[1];
-		String pvcName = args[2];
 
 		logger.debug("config file path: {}", fileName);
+		logger.debug("releaseName: {}", releaseName);
 
 		ConfigBuilder builder = new ConfigBuilder();
 		if (args.length > 1) {
@@ -65,27 +54,16 @@ public class ChartInstallation {
 			logger.debug("kubernetes api version: {}", client.getApiVersion());
 		}
 
-		final URI uri = URI.create("https://kubernetes-charts.storage.googleapis.com/mariadb-2.1.9.tgz");
-		final URL url = uri.toURL();
-		Chart.Builder chart = null;
-		try (final URLChartLoader chartLoader = new URLChartLoader()) {
-			chart = chartLoader.load(url);
-		}
-		assert chart != null;
-
 		final Tiller tiller = new Tiller(client);
 		final ReleaseManager releaseManager = new ReleaseManager(tiller);
 
-		final InstallReleaseRequest.Builder requestBuilder = InstallReleaseRequest.newBuilder();
-		assert requestBuilder != null;
-		requestBuilder.setTimeout(300L);
-		requestBuilder.setName(releaseName);
-		requestBuilder.setWait(true);
-
-		Builder valuesBuilder = requestBuilder.getValuesBuilder();
-		valuesBuilder.setRaw("persistence: \n  existingClaim: " + pvcName);
+		final UninstallReleaseRequest.Builder uninstallRequestBuilder = UninstallReleaseRequest.newBuilder();
+		assert uninstallRequestBuilder != null;
 		
-		final Future<InstallReleaseResponse> releaseFuture = releaseManager.install(requestBuilder, chart);
+		uninstallRequestBuilder.setName(releaseName); // set releaseName
+		uninstallRequestBuilder.setPurge(true); // --purge
+		
+		final Future<UninstallReleaseResponse> releaseFuture = releaseManager.uninstall(uninstallRequestBuilder.build());
 		assert releaseFuture != null;
 		final Release release = releaseFuture.get().getRelease();
 		assert release != null;
